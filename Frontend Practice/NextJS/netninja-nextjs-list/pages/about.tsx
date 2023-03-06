1a90419b6d5124a4305ec43cd378ed08662085e5
Pedr0Leite <pedromiguelgoncalvesleite@gmail.com>
2023-03-06 21:20:01 +0000

    nextjs basic user page

---
+++ Frontend Practice/NextJS/netninja-nextjs-list/pages/about.tsx
@@ -0,0 +1,70 @@
+import Head from "next/head";
+import React from "react";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa ex
+          eveniet saepe dignissimos voluptas, voluptatum laudantium ipsa ipsam
+          quasi perspiciatis reprehenderit assumenda aut earum perferendis, fuga
+          facilis possimus, placeat ullam.
+        </p>
+      </div>
+    </>
+  );
+}
